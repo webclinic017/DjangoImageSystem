e2dc0a3fe0d9f05b74a8e1c50abfa06cb65fea78
Amirbahador Pv <amirbahador.pv@gmail.com>
2021-02-01 12:48:46 -0500

    fix pagination class

--- ImageService/settings/base.py
+++ ImageService/settings/base.py
@@ -119,8 +119,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
-
-    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.AcceptHeaderVersioning',
 
