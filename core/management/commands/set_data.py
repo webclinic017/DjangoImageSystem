2fcf332e8f404151b02db918b42d2ceeb531c2f9
Amirbahador Pv <amirbahador.pv@gmail.com>
2021-02-08 13:20:15 -0500

    add time counter

--- core/management/commands/set_data.py
+++ core/management/commands/set_data.py
@@ -1,6 +1,8 @@
 from django.core.management import BaseCommand
 import json
-
+from ImageService.settings import BASE_DIR
+import time
+import bz2
 """
 python manage.py system_warmup -----> call all command
 """
@@ -11,13 +13,17 @@ class Command(BaseCommand):
            "this will setup picture and slug and etc"
 
     def initial_setup(self):
-        try:
-            with open('../../../data/digikala.txt', 'r') as json_file:
-                my_data = json.load(json_file)
-                print(my_data)
 
-        except Exception() as ex:
-            self.stdout.write(self.style.ERROR(ex))
+        start = time.perf_counter()
+
+        data_address = str(BASE_DIR) + '/data/okala-images.json'
+        with open(data_address, 'r', encoding='utf-8') as json_file:
+            my_data = json.load(json_file)
+            # print(my_data)
+
+        finish = time.perf_counter()
+
+        print(f'Finished in {round(finish-start, 2)} seconds')
 
     def handle(self, *args, **options):
         self.initial_setup()
