813d46382acce4acb1eab1d2f529d01206ba8576
Amirbahador Pv <amirbahador.pv@gmail.com>
2021-02-15 13:35:15 -0500

    coping image

--- core/signals.py
+++ core/signals.py
@@ -2,6 +2,7 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete, pre_delete
 from .models import ProductPicture
 from PIL import Image
+from django.core.files.base import ContentFile
 from django.core import files
 from django.core.files.uploadedfile import InMemoryUploadedFile
 #
@@ -21,35 +22,53 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 #
 #     return slug
 
+# @receiver(post_save, sender=ProductPicture)
+# def pic_change(sender, instance, *args, **kwargs):
+#     if instance.state == 'downloaded':
+#
+#         large_image = Image.open(instance.large.file.name).convert('RGB')
+#         file_name = str(instance.large.name).split('/')[-1]
+#         # large_image.save(large_image, 'webp')
+#         small_image = large_image.copy()
+#         medium_image = large_image.copy()
+#
+#         small_size = (156, 156)
+#         small_pill = small_image.resize(small_size)
+#         instance.small.save(file_name, files.File(small_pill))
+#
+#         # instance.small = all_hi
+#         # instance.small.save(file_name, files.File(all_hi))
+#
+#         medium_size = (182, 182)
+#         medium_pill = medium_image.resize(medium_size)
+#         instance.medium.save(file_name, files.File(medium_pill))
+#
+#         instance.state = 'published'
+#
+#         instance.save()
+#         large_image.close()
+#
+#         print('OK')
+#     else:
+#         print('not OK')
+#         # instance.small = pic_small_gen(instance)
+#         # instance.medium = pic_medium_gen(instance)
 
-@receiver(post_save, sender=ProductPicture)
-def pic_change(sender, instance, *args, **kwargs):
-    if instance.state == 'downloaded':
-
-        large_image = Image.open(instance.large.file.name).convert('RGB')
-        file_name = str(instance.large.name).split('/')[-1]
-        # large_image.save(large_image, 'webp')
-        small_image = large_image.copy()
-        medium_image = large_image.copy()
-
-        small_size = (156, 156)
-        small_pill = small_image.resize(small_size)
-        instance.small.save(file_name, files.File(small_pill))
-
-        # instance.small = all_hi
-        # instance.small.save(file_name, files.File(all_hi))
-
-        medium_size = (182, 182)
-        medium_pill = medium_image.resize(medium_size)
-        instance.medium.save(file_name, files.File(medium_pill))
-
-        instance.state = 'published'
-
-        instance.save()
-        large_image.close()
-
-        print('OK')
-    else:
-        print('not OK')
-        # instance.small = pic_small_gen(instance)
-        # instance.medium = pic_medium_gen(instance)
+#
+# @receiver(post_save, sender=ProductPicture)
+# def pic_change(sender, instance, *args, **kwargs):
+#     if instance.state == 'downloaded':
+#
+#         new_image = ContentFile(instance.large.read())
+#         file_name = str(instance.large.name).split('/')[-1]
+#         new_image.name = file_name + '_s'
+#         instance.small = new_image
+#         instance.save()
+#         new_image.name = file_name + '_m'
+#         instance.medium = new_image
+#         instance.state = 'published'
+#         instance.save()
+#
+#         print('OK')
+#     else:
+#         print('not OK')
